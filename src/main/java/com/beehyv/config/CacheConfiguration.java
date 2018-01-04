8517cf652e7bbc7c97da77e3f165755c27eb8be2
Arpit Gupta <arpit@yahoo.in>
2018-01-04 21:14:11 +0530

    blog components added

--- src/main/java/com/beehyv/config/CacheConfiguration.java
+++ src/main/java/com/beehyv/config/CacheConfiguration.java
@@ -42,6 +42,11 @@ public class CacheConfiguration {
             cm.createCache(com.beehyv.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.beehyv.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.beehyv.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.beehyv.domain.Blog.class.getName(), jcacheConfiguration);
+            cm.createCache(com.beehyv.domain.Entry.class.getName(), jcacheConfiguration);
+            cm.createCache(com.beehyv.domain.Entry.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.beehyv.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.beehyv.domain.Tag.class.getName() + ".entries", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
